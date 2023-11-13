2f58f02997ec974c8d4af97c2e7486ebbce34f9f
Anton <antje12@student.sdu.dk>
2023-11-13 12:18:38 +0100

    CartPole DQN v1

--- dqn-learning-python-ai/main.py
+++ dqn-learning-python-ai/main.py
@@ -11,26 +11,20 @@ import torch
 import torch.nn as nn 
 import torch.optim as optim 
 import torch.nn.functional as F 
-import torchvision.transforms as T 
+import torchvision.transforms as T
 
-class DQN(nn.Module): 
-    def __init__(self, img_height, img_width):
-        super().__init__() 
-        self.fc1 = nn.Linear(in_features=img_height*img_width*3, out_features=24)
-        self.fc2 = nn.Linear(in_features=24, out_features=32)
-        self.out = nn.Linear(in_features=32, out_features=2) 
+class DQN(nn.Module):
+    def __init__(self, observation_space, action_space):
+        super().__init__()
+        self.fc = nn.Linear(in_features=observation_space, out_features=24)
+        self.out = nn.Linear(in_features=24, out_features=action_space)
 
     def forward(self, t):
-        t = t.flatten(start_dim=1)
-        t = F.relu(self.fc1(t))
-        t = F.relu(self.fc2(t))
+        t = F.relu(self.fc(t))
         t = self.out(t)
         return t
 
-Experience = namedtuple(
-    'Experience',
-    ('state', 'action', 'next_state', 'reward')
-)
+Experience = namedtuple('Experience', ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory():
     def __init__(self, capacity):
@@ -71,7 +65,6 @@ class Agent():
     def select_action(self, state, policy_net):
         rate = self.strategy.get_exploration_rate(self.current_step)
         self.current_step += 1
-
         if rate > random.random():
             action = random.randrange(self.num_actions)
             return torch.tensor([action]).to(self.device) # explore
@@ -82,77 +75,32 @@ class Agent():
 class CartPoleEnvManager():
     def __init__(self, device):
         self.device = device
-        self.env = gym.make('CartPole-v1', render_mode="rgb_array").unwrapped
+        self.env = gym.make('CartPole-v1', render_mode="human").unwrapped
         self.env.reset()
-        self.current_screen = None
         self.done = False
 
     def reset(self):
         self.env.reset()
-        self.current_screen = None
 
     def close(self):
         self.env.close()
 
-    def render(self, mode="human"):
-        return self.env.render()#mode)
+    def render(self):
+        return self.env.render()
 
-    def num_actions_available(self):
+    def observation_space(self):
+        return self.env.observation_space.shape[0]
+
+    def action_space(self):
         return self.env.action_space.n
     
     def take_action(self, action):
-        new_state, reward, self.done, truncated, info = self.env.step(action.item())
+        state, reward, self.done, truncated, info = self.env.step(action.item())
         return torch.tensor([reward], device=self.device)
-    
-    def just_starting(self):
-        return self.current_screen is None
 
     def get_state(self):
-        if self.just_starting() or self.done:
-            self.current_screen = self.get_processed_screen()
-            black_screen = torch.zeros_like(self.current_screen)
-            return black_screen
-        else:
-            s1 = self.current_screen
-            s2 = self.get_processed_screen()
-            self.current_screen = s2
-            return s2 - s1
-
-    def get_screen_height(self):
-        screen = self.get_processed_screen()
-        return screen.shape[2]
-
-    def get_screen_width(self):
-        screen = self.get_processed_screen()
-        return screen.shape[3]
-    
-    def get_processed_screen(self):
-        screen = self.render("rgb_array").transpose((2, 0, 1))
-        screen = self.crop_screen(screen)
-        return self.transform_screen_data(screen)
-
-    def crop_screen(self, screen):
-        screen_height = screen.shape[1]
-
-        # Strip off top and bottom
-        top = int(screen_height * 0.4)
-        bottom = int(screen_height * 0.8)
-        screen = screen[:, top:bottom, :]
-        return screen
-
-    def transform_screen_data(self, screen):
-        # Convert to float, rescale, convert to tensor
-        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
-        screen = torch.from_numpy(screen)
-
-        # Use torchvision package to compose image transforms
-        resize = T.Compose([
-            T.ToPILImage(),
-            T.Resize((40, 90)),
-            T.ToTensor()
-        ])
-
-        return resize(screen).unsqueeze(0).to(self.device) # add a batch dimension
+        state = torch.tensor(self.env.state, dtype=torch.float32, device=self.device)
+        return state.view(1, -1)  # Reshape to (1, 4) tensor
 
 def plot(values, moving_avg_period):
     plt.figure(2)
@@ -161,13 +109,11 @@ def plot(values, moving_avg_period):
     plt.xlabel("Episode")
     plt.ylabel("Duration")
     plt.plot(values)
-    
     moving_avg = get_moving_average(moving_avg_period, values)
     plt.plot(moving_avg)
     plt.pause(0.001)
     print("Episode", len(values), "\n", \
         moving_avg_period, "episode moving avg:", moving_avg[-1])
-    #if is_ipython: display.clear_output(wait=True)
 
 def get_moving_average(period, values):
     values = torch.tensor(values, dtype=torch.float)
@@ -182,12 +128,10 @@ def get_moving_average(period, values):
 
 def extract_tensors(experiences):
     batch = Experience(*zip(*experiences))
-
     t1 = torch.cat(batch.state)
     t2 = torch.cat(batch.action)
     t3 = torch.cat(batch.reward)
     t4 = torch.cat(batch.next_state)
-
     return (t1, t2, t3, t4)
 
 class QValues():
@@ -221,11 +165,16 @@ num_episodes = 1000
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 em = CartPoleEnvManager(device)
 strategy = EpsilonGreedyStrategy(eps_start, eps_end, eps_decay)
-agent = Agent(strategy, em.num_actions_available(), device)
+agent = Agent(strategy, em.action_space(), device)
 memory = ReplayMemory(memory_size)
 
-policy_net = DQN(em.get_screen_height(), em.get_screen_width()).to(device)
-target_net = DQN(em.get_screen_height(), em.get_screen_width()).to(device)
+print("----------")
+print("observation space: ", em.observation_space())
+print("action space: ", em.action_space())
+print("----------")
+
+policy_net = DQN(em.observation_space(), em.action_space()).to(device)
+target_net = DQN(em.observation_space(), em.action_space()).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 optimizer = optim.Adam(params=policy_net.parameters(), lr=lr)
@@ -245,11 +194,9 @@ for episode in range(num_episodes):
         if memory.can_provide_sample(batch_size):
             experiences = memory.sample(batch_size)
             states, actions, rewards, next_states = extract_tensors(experiences)
-
             current_q_values = QValues.get_current(policy_net, states, actions)
             next_q_values = QValues.get_next(target_net, next_states)
             target_q_values = (next_q_values * gamma) + rewards
-
             loss = F.mse_loss(current_q_values, target_q_values.unsqueeze(1))
             optimizer.zero_grad()
             loss.backward()
