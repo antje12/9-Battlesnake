51d3330dd93f79c11ea072a2823f36b748094e21
Anton <antje12@student.sdu.dk>
2023-12-01 12:24:34 +0100

    DQN player setup

--- dqn-playing-python-ai/main.py
+++ dqn-playing-python-ai/main.py
@@ -12,152 +12,15 @@
 
 import random
 import typing
-from math import sqrt
 
-import numpy as np 
-import matplotlib 
-matplotlib.use('TkAgg') 
-import matplotlib.pyplot as plt 
- 
-import torch 
-import torch.nn as nn 
-import torch.nn.functional as F 
-import torch.optim as optim 
-
-from battlesnake_gym.snake_gym import BattlesnakeGym 
-from battlesnake_gym.snake import Snake 
-
-env = BattlesnakeGym(map_size=(11, 11), number_of_snakes=1)
-observation_space = env.observation_space.shape
-action_space = env.action_space[0].n
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-EPISODES = 1000
-LEARNING_RATE = 0.0005
-
-MEM_SIZE = 20000
-BATCH_SIZE = 128
-GAMMA = 0.98
-
-EXPLORATION_MAX = 1.0
-EXPLORATION_DECAY = 0.85
-EXPLORATION_MIN = 0.001
-
-HIDDEN_LAYER1_DIMS = 256
-HIDDEN_LAYER2_DIMS = 128
-
-class Network(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        input_size = np.prod(observation_space)
-        self.fc1 = nn.Linear(input_size, HIDDEN_LAYER1_DIMS)
-        self.fc2 = nn.Linear(HIDDEN_LAYER1_DIMS, HIDDEN_LAYER2_DIMS)
-        self.fc3 = nn.Linear(HIDDEN_LAYER2_DIMS, action_space)
-
-        self.optimizer = optim.Adam(self.parameters(), lr=LEARNING_RATE)
-        self.loss = nn.MSELoss()
-        self.to(DEVICE)
-    
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-class ReplayBuffer:
-    def __init__(self):
-        self.mem_count = 0
-        
-        input_size = np.prod(observation_space)
-        self.states = np.zeros((MEM_SIZE, input_size),dtype=np.float32)
-        self.actions = np.zeros(MEM_SIZE, dtype=np.int64)
-        self.rewards = np.zeros(MEM_SIZE, dtype=np.float32)
-        self.states_ = np.zeros((MEM_SIZE, input_size),dtype=np.float32)
-        self.dones = np.zeros(MEM_SIZE, dtype=bool)
-    
-    def add(self, state, action, reward, state_, done):
-        mem_index = self.mem_count % MEM_SIZE
-        
-        self.states[mem_index]  = state
-        self.actions[mem_index] = action
-        self.rewards[mem_index] = reward
-        self.states_[mem_index] = state_
-        self.dones[mem_index] =  1 - done
-
-        self.mem_count += 1
-    
-    def sample(self):
-        MEM_MAX = min(self.mem_count, MEM_SIZE)
-        batch_indices = np.random.choice(MEM_MAX, BATCH_SIZE, replace=True)
-        
-        states  = self.states[batch_indices]
-        actions = self.actions[batch_indices]
-        rewards = self.rewards[batch_indices]
-        states_ = self.states_[batch_indices]
-        dones   = self.dones[batch_indices]
-
-        return states, actions, rewards, states_, dones
-
-class DQN_Solver:
-    def __init__(self, model):
-        self.memory = ReplayBuffer()
-        self.exploration_rate = 0
-        self.network = model
-
-    def choose_action(self, observation):
-        if random.random() < self.exploration_rate:
-            #return random.randint(0, 0)
-            return env.action_space[0].sample()
-        
-        state = torch.tensor(observation).float().detach()
-        state = state.to(DEVICE)
-        state = state.unsqueeze(0)
-        q_values = self.network(state)
-        return torch.argmax(q_values).item()
-    
-    def learn(self):
-        if self.memory.mem_count < BATCH_SIZE:
-            return
-        
-        states, actions, rewards, states_, dones = self.memory.sample()
-        states = torch.tensor(states , dtype=torch.float32).to(DEVICE)
-        actions = torch.tensor(actions, dtype=torch.long).to(DEVICE)
-        rewards = torch.tensor(rewards, dtype=torch.float32).to(DEVICE)
-        states_ = torch.tensor(states_, dtype=torch.float32).to(DEVICE)
-        dones = torch.tensor(dones, dtype=torch.bool).to(DEVICE)
-        batch_indices = np.arange(BATCH_SIZE, dtype=np.int64)
-
-        q_values = self.network(states)
-        next_q_values = self.network(states_)
-
-        predicted_value_of_now = q_values[batch_indices, actions]
-        predicted_value_of_future = torch.max(next_q_values, dim=1)[0]
-        
-        q_target = rewards + GAMMA * predicted_value_of_future * dones
-
-        loss = self.network.loss(q_target, predicted_value_of_now)
-        self.network.optimizer.zero_grad()
-        loss.backward()
-        self.network.optimizer.step()
-
-        self.exploration_rate *= EXPLORATION_DECAY
-        self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
-
-    def returning_epsilon(self):
-        return self.exploration_rate
-
-def save_model(model, filepath):
-    torch.save(model.state_dict(), filepath)
-    print(f"Model saved to {filepath}")
-
-def load_model(filepath): 
-    model = Network() 
-    model.load_state_dict(torch.load(filepath)) 
-    print(f"Model loaded from {filepath}") 
-    return model
-
-agent = DQN_Solver(load_model("single_261.pth"))
+import numpy as np  
+import matplotlib  
+matplotlib.use('TkAgg')  
+  
+import torch  
+import torch.nn as nn  
+import torch.nn.functional as F  
+import torch.optim as optim  
 
 # info is called when you create your Battlesnake on play.battlesnake.com
 # and controls your Battlesnake's appearance
@@ -179,7 +42,68 @@ def start(game_state: typing.Dict):
 
 # end is called when your Battlesnake finishes a game
 def end(game_state: typing.Dict):
-    print("GAME OVER\n")
+    print("GAME OVER")
+
+# DQN code >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Game board
+features = 3
+height = 11
+width = 11
+observation_space = (features, height, width) 
+action_space = 4 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+
+# Hyperparameter
+LEARNING_RATE = 0.0005 
+HIDDEN_LAYER1_DIMS = 32
+HIDDEN_LAYER2_DIMS = 64
+HIDDEN_LAYER3_DIMS = 256
+HIDDEN_LAYER4_DIMS = 128
+
+# Agent Code
+class Network(torch.nn.Module): 
+    def __init__(self): 
+        super().__init__() 
+        input_channels = features  # Number of channels in the input (e.g., features for each cell)
+        # 2D array processing (spatial local 5*5 info processing)
+        self.conv1 = nn.Conv2d(input_channels, HIDDEN_LAYER1_DIMS, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(HIDDEN_LAYER1_DIMS, HIDDEN_LAYER2_DIMS, kernel_size=5, stride=1, padding=2)
+        # 1D array processing (global info processing)
+        self.fc3 = nn.Linear(HIDDEN_LAYER2_DIMS * height * width, HIDDEN_LAYER3_DIMS) 
+        self.fc4 = nn.Linear(HIDDEN_LAYER3_DIMS, HIDDEN_LAYER4_DIMS)
+        self.out = nn.Linear(HIDDEN_LAYER4_DIMS, action_space) 
+        self.optimizer = optim.Adam(self.parameters(), lr=LEARNING_RATE) 
+        self.loss = nn.MSELoss() 
+        self.to(DEVICE) 
+    
+    def forward(self, x): 
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, HIDDEN_LAYER2_DIMS * height * width)  # Flatten the output of the convolutional layers
+        x = F.relu(self.fc3(x)) 
+        x = F.relu(self.fc4(x)) 
+        x = self.out(x) 
+        return x 
+
+class DQN_Solver: 
+    def __init__(self, model): 
+        self.network = model
+ 
+    def choose_action(self, observation):          
+        state = torch.tensor(observation).float().detach() 
+        state = state.to(DEVICE) 
+        state = state.unsqueeze(0) 
+        q_values = self.network(state) 
+        return torch.argmax(q_values).item() 
+
+def load_model(filepath):  
+    model = Network()
+    model.load_state_dict(torch.load(filepath))  
+    print(f"Model loaded from {filepath}")  
+    return model
+
+agent = DQN_Solver(load_model("final_model.pth"))
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DQN code
 
 # move is called on every turn and returns your next move
 # Valid moves are "up", "down", "left", or "right"
@@ -189,27 +113,13 @@ def move(game_state: typing.Dict) -> typing.Dict:
     moves = ["up", "down", "left", "right"]
     next_move = random.choice(moves)
 
-    board = game_state['board']
+    #board = game_state['board']
     my_head = game_state["you"]["body"][0] # Coordinates of your head
     foods = game_state['board']['food']
     hazards = game_state['board']['hazards']
     opponents = game_state['board']['snakes']
 
-    map = np.zeros((11, 11), dtype=int)
-    for oponent in opponents:
-        for part in oponent["body"]:
-            x, y = part["x"], part["y"]
-            map[x, y] = 1
-    for hazard in hazards:
-        x, y = hazard["x"], hazard["y"]
-        map[x, y] = 1
-    
-    for food in foods:
-        x, y = food["x"], food["y"]
-        map[x, y] = 2
-    
-    x, y = my_head["x"], my_head["y"]
-    map[x, y] = 3
+    state = extract_state(my_head, foods, hazards, opponents)
 
     # 0 to 3 corresponding to Snake.UP, Snake.DOWN, Snake.LEFT, Snake.RIGHT 
     action = agent.choose_action(state)
@@ -222,8 +132,34 @@ def move(game_state: typing.Dict) -> typing.Dict:
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
 
+def extract_state(my_head, foods, hazards, opponents):
+    map_array = np.zeros((11, 11, 3), dtype=int)
+
+    # Danger cells
+    for oponent in opponents: # Includes me
+        for part in oponent["body"]:
+            x, y = part["x"], part["y"]
+            map_array[x, y, 1] = 1
+    for hazard in hazards:
+        x, y = hazard["x"], hazard["y"]
+        map_array[x, y, 1] = 1
+
+    # Food cells
+    for  food in foods:
+        x, y = food["x"], food["y"]
+        map_array[x, y, 2] = 1
+
+    # Player cell
+    x, y = my_head["x"], my_head["y"]
+    map_array[x, y, 0] = 1 # Set head
+    map_array[x, y, 1] = 0 # Clear dangers
+    map_array[x, y, 2] = 0 # Clear food
+            
+    # Add batch dimension
+    map_array = map_array.reshape(features, height, width)
+    return map_array
+
 # Start server when `python main.py` is run
 if __name__ == "__main__":
     from server import run_server
-
     run_server({"info": info, "start": start, "move": move, "end": end})
