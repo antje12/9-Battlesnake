c5706700f7750fe30063d21a58f48163257a4c7b
Anton <antje12@student.sdu.dk>
2023-12-02 16:34:10 +0100

    multi run

--- battlesnake/multi-run.cmd
+++ battlesnake/multi-run.cmd
@@ -1,1 +1,1 @@
-battlesnake play -W 11 -H 11 --name TestSnake1 --url http://localhost:8000 --name TestSnake2 --url http://localhost:8000 -v
+battlesnake play -W 11 -H 11 --name TestSnake1 --url http://localhost:8000 --name TestSnake2 --url http://localhost:8000 --name TestSnake3 --url http://localhost:8000 --name TestSnake4 --url http://localhost:8000 -v
